4c687f5363bdacf0b50e3dc13f7b41e9e7bb04f6
Nikolay Savchenko <7548568@gmail.com>
2023-03-24 22:59:20 +0300

    Update dafault

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Places(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    description_short = models.TextField('Краткое описание')
-    description_long = HTMLField('Описание точки')
+    description_short = models.TextField('Краткое описание', blank=True)
+    description_long = HTMLField('Описание точки', blank=True)
     longitude = models.FloatField('Долгота:')
     latitude = models.FloatField('Широта:')
 
@@ -17,7 +17,7 @@ class Images(models.Model):
     place = models.ForeignKey(Places, related_name='images',
                               on_delete=models.CASCADE)
     image = models.ImageField(upload_to='media/')
-    position = models.PositiveIntegerField('Позиция к показу', default=99,
+    position = models.PositiveIntegerField('Позиция к показу', default=0,
                                            blank=True, null=True)
 
     def __str__(self):
