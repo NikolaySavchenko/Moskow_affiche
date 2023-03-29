2bde4583b168d9895759c95b054dcb7dda2f99a9
Nikolay Savchenko <7548568@gmail.com>
2023-03-29 22:00:12 +0300

    Rendered the function preview_image

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from django.utils.safestring import mark_safe
 from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 
 from .secondary_tools import preview_image
@@ -10,16 +8,8 @@ from .models import Places, Images
 
 class ImagesStackedInline(SortableStackedInline):
     model = Images
-    readonly_fields = ['preview_image']
-
-    def preview_image(self, picture):
-        return format_html(
-            mark_safe('<img src="{url}" height={height} />'),
-            url=picture.image.url,
-            height=200,
-        )
-
-    fields = ('image', 'preview_image', 'position')
+    readonly_fields = (preview_image,)
+    fields = ('image', preview_image, 'position')
 
 
 @admin.register(Places)
@@ -29,11 +19,4 @@ class SortablePlacesAdmin(SortableAdminBase, admin.ModelAdmin):
 
 @admin.register(Images)
 class ImagesAdmin(admin.ModelAdmin):
-    readonly_fields = ['preview_image']
-
-    def preview_image(self, picture):
-        return format_html(
-            mark_safe('<img src="{url}" height={height} />'),
-            url=picture.image.url,
-            height=200,
-        )
+    readonly_fields = (preview_image,)
--- places/secondary_tools.py
+++ places/secondary_tools.py
@@ -2,7 +2,7 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
 
-def preview_image(self, picture):
+def preview_image(picture):
     return format_html(
         mark_safe('<img src="{url}" height={height} />'),
         url=picture.image.url,
